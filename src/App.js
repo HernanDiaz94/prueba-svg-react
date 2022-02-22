fc5bf34b71e5235a0f02de271c75470e3758db47
Hernan Diaz <hernan.diaz@phinxlab.com>
2022-02-21 21:17:10 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Background1 from './assents/background1';
+import Background2 from './assents/background2';
+/* import Background3 from './assents/background3'; */
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Background1 />
+        <Background2 />
+        {/* <Background3 /> */}
       </header>
     </div>
   );
